95a499308cc0c0fdbc6f53d937ad051b0d820140
Jason Liu <oiabot2003@outlook.com>
2020-07-07 16:13:46 +0800

    clear the errors shown at the end of the execution

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ token = "{GET-TOKEN-FROM-LOGINED-NOTION'S-COOKIE}"
 url = "{NOTION-PAGE-URL}"
 
 
-def initNotionPage():
-  client = NotionClient(token_v2=token)
-  page = client.get_block(url)
-  return page
+def getBlockIndex(block):
+    try:
+        index = re.findall(pattern, block)[0]
+        return index
+    except IndexError as e:
+        print('Index Error occurred')
 
 
 def getBlockIndex(block):
